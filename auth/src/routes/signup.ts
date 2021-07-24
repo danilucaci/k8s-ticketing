d87d4f9bd6587e4e338b65ed8c2956460898c172
Dani Lucaci <danilucaci@gmail.com>
2021-07-24 15:57:28 +0200

    chore: add section 8 finished code

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -14,16 +15,27 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("The password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
 
-    res.status(200).send("Hello");
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send({
+      user,
+    });
   }
 );
 
