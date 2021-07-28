6048bfdece44ade70e10e79c4975ca04ff954e5b
Dani Lucaci <danilucaci@gmail.com>
2021-07-28 19:46:47 +0200

    chore: add package config

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,1 +1,13 @@
-const color: string = "hola";
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 10,
+  green: 10,
+  blue: 10,
+};
+
+export default color;
